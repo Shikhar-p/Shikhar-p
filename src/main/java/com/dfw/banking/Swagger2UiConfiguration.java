b7e9881592abf719280a129c30099dd664a5b47c
Shikhar-p <shikharp.93@gmail.com>
2021-08-15 13:19:27 +0530

    modified in prop file and swagger file

--- src/main/java/com/dfw/banking/Swagger2UiConfiguration.java
+++ src/main/java/com/dfw/banking/Swagger2UiConfiguration.java
@@ -23,7 +23,7 @@ public class Swagger2UiConfiguration extends WebMvcConfigurationSupport
         //Also it is configuring the Swagger Docket
         return new Docket(DocumentationType.SWAGGER_2).select()
                 // .apis(RequestHandlerSelectors.any())
-                .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
+                .apis(Predicates.not(RequestHandlerSelectors.basePackage("com.dfw.banking")))
                 // .paths(PathSelectors.any())
                 // .paths(PathSelectors.ant("/swagger2-demo"))
                 .build();
